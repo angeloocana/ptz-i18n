40ed1683321ae0c8bf3ab5902edd6f35ebe5783b
Angelo Ocana <angeloocana@gmail.com>
2017-10-12 16:31:16 -0400

    Remove console.log

--- src/redirectToHome.js
+++ src/redirectToHome.js
@@ -4,14 +4,12 @@ import getUserLangKey from './getUserLangKey';
  * Redirect to user language home page, from: / to: /langKey/
  * @param {[String]} langs allowed lang keys ['en', 'fr', 'pt']
  * @param {String} defaultLangKey default browser language key
- * @return {string} newUrl
+ * @return {void}
  */
 const redirectToHome = (langs, defaultLangKey) => {
   const langKey = getUserLangKey(langs, defaultLangKey);
   const newUrl = `/${langKey}/`;
-  console.log('newUrl:', newUrl);
   window.location.replace(newUrl);
-  return newUrl;
 };
 
 export default redirectToHome;
